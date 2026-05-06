04193865c23e1f92fe7d068ae413245993557b31
Isabela de Oliveira <isabela_oliveira3@estudante.sesisenai.org.br>
2026-05-05 21:55:49 -0300

    add - visualizador de senha

--- script/validacao.js
+++ script/validacao.js
@@ -1,38 +1,54 @@
 const login = document.querySelector(".form1");
 const elMensagem = document.getElementById("mensagem");
 
+const toggleSenha = document.getElementById("toggleSenha");
+const senha = document.getElementById("inputSenha");
+const iconSenha = toggleSenha.querySelector("i");
+
+if (toggleSenha) {
+    toggleSenha.onclick = () => {
+        if (senha.type === "password") {
+            senha.type = "text";
+            iconSenha.className = "bi bi-eye-slash";
+        } else {
+            senha.type = "password";
+            iconSenha.className = "bi bi-eye";
+        }
+    };
+}
+
 if (login) {
 
     login.onsubmit = function (event) {
         event.preventDefault();
 
         const email = document.getElementById("inputEmail").value;
-        const senha = document.getElementById("inputSenha").value;
+        const senhaValue = inputSenha.value;
 
-        const email_definido = "admin@teste.com";
-        const senha_definida = "adm1n123";
+        const emailDefinido = "admin@teste.com";
+        const senhaDefinida = "adm1n123";
 
         elMensagem.innerHTML = "";
 
-        if (email === "" || senha === "") {
-            elMensagem.innerHTML = "<div class = 'text-danger fw-bold'> Preencha todos os campos</div>";
+        if (email === "" || senhaValue === "") {
+            elMensagem.innerHTML = "<div class='text-danger fw-bold'>Preencha todos os campos</div>";
             return;
         }
 
         if (!email.includes("@") || !email.includes(".")) {
-            elMensagem.innerHTML = "<div class = 'text-danger fw-bold'> Email Inválido!</div>";
+            elMensagem.innerHTML = "<div class='text-danger fw-bold'>Email Inválido!</div>";
             return;
         }
 
         if (senha.length < 4) {
-            elMensagem.innerHTML = "<div class = 'text-danger fw-bold'> Senha muito curta!</div>";
+            elMensagem.innerHTML = "<div class='text-danger fw-bold'>Senha muito curta!</div>";
             return
         }
 
-        if (email === email_definido && senha === senha_definida) {
+        if (email === emailDefinido && senhaValue === senhaDefinida) {
             window.location.href = "tela_geral_home.html";
-        }else{
-            elMensagem.innerHTML = "<div class = 'text-danger fw-bold'>Dados incorretos!</div>";
+        } else {
+            elMensagem.innerHTML = "<div class='text-danger fw-bold'>Dados incorretos!</div>";
         }
     };
 }

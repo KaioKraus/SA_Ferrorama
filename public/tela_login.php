8c0b34a640d11bf60c56cc71ef79f2b6460b5057
Davi-Freitass <davi_f_freitas@estudante.sesisenai.org.br>
2026-09-09 12:45:51 -0300

    fix - correção botão de visualização de senha na tela de login

--- public/tela_login.php
+++ public/tela_login.php
@@ -102,6 +102,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
     </form>
 
+    <script src="../script/validacao.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
